1ad366b025b2af3cb847f28afa543cdc7a73eb18
Psivakrishnadev <prabala.siva12@gmail.com>
2022-07-18 11:16:23 +0530

    Week 4 Assignment

---
+++ Week-4/4.2/index.js
@@ -0,0 +1,16 @@
+var Person = function() {};
+
+Person.prototype.initialize = function(name, age) { 
+    this.name = name; 
+    this.age = age; 
+}
+
+var Teacher = function() { 
+    this.teach = function(subject){ 
+        console.log(this.name + " is now teaching " + subject);
+    } 
+} 
+Teacher.prototype = new Person(); 
+var him = new Teacher();
+
+him.initialize("Shashank", 26); him.teach("Inheritance");
